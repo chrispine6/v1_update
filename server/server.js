42d8329fcb69cfed1c00af6616e2e0750842410e
chrispine6 <88315170+chrispine6@users.noreply.github.com>
2024-06-21 23:36:21 +0530

    cant get next ui to work

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
-//server.js
-
 const http = require('http');
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -9,30 +8,16 @@ const passport = require('passport');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user.model');
-const Event = require('./models/event.model');
-const Message = require('./models/message.model');
-const authRoutes = require('./routes/auth.routes');
-const homeRoutes = require('./routes/home.routes');
-const messageRoutes = require('./routes/message.routes');
-const profileRoutes = require('./routes/profile.routes');
-const communityRoutes = require('./routes/community.routes');
-const postRoutes = require('./routes/post.routes'); 
-const eventRoutes = require('./routes/event.routes');
-const searchRoutes = require('./routes/search.routes');
-const notificationRoutes = require('./routes/notification.routes');
-const calendarRoutes = require('./routes/calendar.routes');
-const premiumRoutes = require('./routes/premium.routes');
-const accountType = require('./routes/accounttype.routes');
-const organizationRoutes = require('./routes/organization.routes');
+
+const landingRoutes = require('./src/routes/landing.routes');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}/`);
 });
 
 app.use(session({
@@ -44,16 +29,16 @@ app.use(session({
 io.on('connection', socket => {
   console.log('New client connected');
   socket.on('disconnect', () => {
-      console.log('Client disconnected');
+    console.log('Client disconnected');
   });
 });
 
-mongoose.connect('mongodb+srv://chris:c5LUJn643HvqAFx@dialogueprototype0clust.eoas6vw.mongodb.net/').then(() => console.log('MongoDB Connected'))
+mongoose.connect('mongodb+srv://chris:c5LUJn643HvqAFx@dialogueprototype0clust.eoas6vw.mongodb.net/')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.set('view engine', 'ejs');
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -94,22 +79,10 @@ app.use((req, res, next) => {
   next();
 });
 
-// css
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', authRoutes);
-app.use('/home', homeRoutes);
-app.use('/conversations', (req, res, next) => {
-  req.io = io;
-  next();
-}, messageRoutes);
-app.use('/profile', profileRoutes);
-app.use('/community', communityRoutes);
-app.use('/post', postRoutes);
-app.use('/event', eventRoutes);
-app.use('/search', searchRoutes);
-app.use('/notification', notificationRoutes);
-app.use('/calendar', calendarRoutes);
-app.use('/premium', premiumRoutes);
-app.use('/account', accountType);
-app.use('/organization', organizationRoutes);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+app.use('/', landingRoutes);
---
+++ server/src/routes/auth.routes.js
@@ -0,0 +1,75 @@
+// routes/auth.routes.js
+
+const express = require('express');
+const User = require('../models/user.model');
+const passport = require('passport');
+const router = express.Router();
+const checkFirstTimeLogin = require('../middleware/hasLoggedInBefore');
+
+// // initial route
+// router.get('/', (req, res) => {
+//   res.render('../client/index.ejs');
+// });
+
+// Render registration form
+router.get('/register', (req, res) => {
+  res.render('registration');
+});
+
+// post register
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, phone, dob, username, password } = req.body;
+    let user = await User.findOne({ email: email });
+    if (user) {
+      return res.status(400).send('User already registered with this email.');
+    }
+
+    user = new User({
+      name,
+      email,
+      phone,
+      dob,
+      username,
+      password
+    });
+
+    await user.save();
+
+    res.redirect('/login')
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error during registration');
+  }
+});
+
+// Render login form
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Login route
+router.post('/login', passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: false
+}), checkFirstTimeLogin, (req, res) => {
+  if (req.session.redirectToNotifications) {
+    delete req.session.redirectToNotifications;
+    res.redirect('/notifications');
+  } else {
+    res.redirect('/home');
+  }
+});
+
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+      if (err) {
+          return next(err);
+      }
+      res.redirect('/');
+  });
+});
+
+module.exports = router;
